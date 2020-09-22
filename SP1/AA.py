d3cd874ca100c91ccf13566a9fc769e4f0784a76
minsu <minsu.kim08@gmail.com>
2020-09-22 19:41:18 +0900

    k번째 큰 수

--- SP1/AA.py
+++ SP1/AA.py
@@ -25,3 +25,17 @@ for t in range(T):
     a.sort()
     print("#%d %d" %(t+1,a[k-1]))
 '''
+'''
+k번째 큰수
+
+n, m=map(int,input().split())
+a=list(map(int,input().split()))
+res=set()
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            res.add(a[i]+a[j]+a[k])
+res=list(res)
+res.sort(reverse=True)
+print(res[m-1])
+'''
